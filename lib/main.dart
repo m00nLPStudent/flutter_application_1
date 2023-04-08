9381fc6d8991f53727040bcf647d6f11edefd07e
Swen J. Verbocket <s.verbocket@yahoo.com>
2023-04-09 00:56:23 +0200

    bilder hinzugefügt

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -97,7 +96,52 @@ class _CardPileState extends State<CardPile> {
     '18.png',
     '19.png',
     '20.png',
-    '21.png'
+    '21.png',
+    '22.png',
+    '23.png',
+    '24.png',
+    '25.png',
+    '26.png',
+    '27.png',
+    '28.png',
+    '29.png',
+    '30.png',
+    '31.png',
+    '32.png',
+    '33.png',
+    '34.png',
+    '35.png',
+    '36.png',
+    '37.png',
+    '38.png',
+    '39.png',
+    '40.png',
+    '41.png',
+    '42.png',
+    '43.png',
+    '44.png',
+    '45.png',
+    '46.png',
+    '47.png',
+    '48.png',
+    '49.png',
+    '50.png',
+    '51.png',
+    '52.png',
+    '53.png',
+    '54.png',
+    '55.png',
+    '56.png',
+    '57.png',
+    '58.png',
+    '59.png',
+    '60.png',
+    '61.png',
+    '62.png',
+    '63.png',
+    '64.png',
+    '65.png'
+
   ];
   String selectedCard = '';
 
