9034f92f9c000e176598c11b2ee7d28056eb2401
Swen J. Verbocket <s.verbocket@yahoo.com>
2023-04-10 21:02:15 +0200

    Fomart adapted for larger display

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,5 @@
-import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -10,13 +11,11 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+  late String _videoFileName;
 
   @override
   void initState() {
     super.initState();
-
-    _controller = VideoPlayerController.asset('videos/splash.mp4');
-    _initializeVideoPlayerFuture = _controller.initialize();
   }
 
   @override
@@ -27,23 +26,31 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    if (screenWidth < 600) {
+      _videoFileName = 'videos/splash_mobile_small.mp4';
+    } else if (screenWidth < 1200) {
+      _videoFileName = 'videos/splash_mobile.mp4';
+    } else {
+      _videoFileName = 'videos/splash_desktop.mp4';
+    }
+
+    _controller = VideoPlayerController.asset(_videoFileName);
+    _initializeVideoPlayerFuture = _controller.initialize();
+
     return FutureBuilder(
       future: _initializeVideoPlayerFuture,
       builder: (context, snapshot) {
-        //print("Connection state: ${snapshot.connectionState}");
         if (snapshot.connectionState == ConnectionState.done) {
           _controller.play();
           return Container(
             color: Colors.white,
-            child: AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: SizedBox(
-                  width: _controller.value.size.width ?? 0,
-                  height: _controller.value.size.height ?? 0,
-                  child: VideoPlayer(_controller),
-                ),
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
               ),
             ),
           );
