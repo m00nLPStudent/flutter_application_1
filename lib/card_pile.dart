35732fb77980e367beea1af7d80d2bef8b64778e
Swen J. Verbocket <s.verbocket@yahoo.com>
2023-04-11 01:47:01 +0200

    anpssung am Designe footer und head bereich angepasst

--- lib/card_pile.dart
+++ lib/card_pile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'card_description.dart';
 import 'card_translation.dart';
+import 'card_list.dart';
+
 
 class CardPile extends StatefulWidget {
   @override
@@ -9,80 +11,9 @@ class CardPile extends StatefulWidget {
 }
 
 class _CardPileState extends State<CardPile> {
-  final karten = [
-    'der_agile_professor.png',
-    'der_aktive.png',
-    'der_alles_an_sich_ziehende.png',
-    'der_auf_und_abbau.png',
-    'die_bewusste_wandlung.png',
-    'das_chaotische_glueck.png',
-    'der_denker.png',
-    'der_dirigent_des_lebens.png',
-    'die_dummheit.png',
-    'der_durchstarter.png',
-    'der_energetische_fluss.png',
-    'der_entmachtete.png',
-    'der_emotionale_sicherheitgeber.png',
-    'der_eroberer.png',
-    'der_flexible.png',
-    'der_gefuehlsdisziplinierte.png',
-    'der_gefuehlsdirektor.png',
-    'der_gefuehlsmensch.png',
-    'der_gefuehlvolle_sprinter.png',
-    'der_gefuehlvolle_stuermer.png',
-    'der_gesicherte_erfolg.png',
-    'der_geteuerte_weitblick.png',
-    'die_glueckseligkeit.png',
-    'das_glueckskind.png',
-    'der_glueckspilz.png',
-    'der_großunternehmer.png',
-    'der_haeuslebauer.png',
-    'die_herzensbrecherin.png',
-    'die_inspiration.png',
-    'der_instabile.png',
-    'die_irrefuehrung.png',
-    'der_kampf_zwischen_gefuehl_und_trophaee.png',
-    'das_kopf-gefuehlschaos.png',
-    'das_kreative_genie.png',
-    'der_kreative_kuenstler.png',
-    'der_kuenstler_im_absoluten.png',
-    'die_logische_beziehung.png',
-    'der_logische_energieeinsatz.png',
-    'der_logische_kalkulator.png',
-    'das_materialliserte_glueck.png',
-    'der_partnerwechsel.png',
-    'der_powerstart.png',
-    'der_powerstart.png',
-    'die_sichtbare_stabilitaet.png',
-    'der_spirituelle_alchemist.png',
-    'der_stabile_greis.png',
-    'der_stabile.png',
-    'der_stolze_king_of_life.png',
-    'der_stolze_repraesentant.png',
-    'der_traeumer.png',
-    'die_traeumerische_sichtweise.png',
-    'der_unbewegliche.png',
-    'der_unflexible.png',
-    'der_ungeliebte.png',
-    'der_unglueckliche.png',
-    'der_unsichere.png',
-    'der_verblendete.png',
-    'die_versuchung.png',
-    'der_verwandlungskuenstler.png',
-    'der_visuelle_chaot.png',
-    'der_wandler.png',
-    'der_wissenschftliche_gluecksbringer.png',
-    'der_zielbewusste_neureiche.png'
-  ];
+  final karten = card_list;
   String selectedCard = '';
 
-  List<String> menuItems = [
-    'Element 1',
-    'Element 2',
-    'Element 3',
-  ];
-
-  String selectedItem = 'Element 1';
   Translation translation = Translation(entries: {});
 
   void _readFile() async {
@@ -104,90 +35,192 @@ class _CardPileState extends State<CardPile> {
     double screenHeight = MediaQuery.of(context).size.height;
     _readFile();
 
+    double cardWidth = screenWidth * 0.6;
+    double cardHeight = screenHeight * 0.4;
+    double left = (screenWidth - cardWidth) / 2;
+    double top = (screenHeight - cardHeight) / 2;
+
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Opalia - Numerologiekarten'),
-        leading: PopupMenuButton(
-          itemBuilder: (BuildContext context) {
-            return menuItems.map((String item) {
-              return PopupMenuItem(
-                value: item,
-                child: Text(item),
-              );
-            }).toList();
+        title: null,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.home, size: 40,),
+          onPressed: () {
+            // Behandeln Sie den Klick auf den Navigationsknopf
           },
-          onSelected: (String newValue) {
-            setState(() {
-              selectedItem = newValue;
-            });
-          },
-          offset: const Offset(-100, 0),
         ),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/background.gif'),
-            fit: BoxFit.cover,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.history, size: 40),
+
+            onPressed: () {
+              // Behandeln Sie den Klick auf die Suchschaltfläche
+            },
           ),
-        ),
-        child: Stack(
-          children: [
-            // Anzeigen der Kartenrückseite
-            ...karten.map((kartenname) {
-              final randomAngle = Random().nextInt(90) - 45;
-              final randomWidth = Random().nextInt(80) + 140;
-              final randomHeight = Random().nextInt(80) + 200;
-              final left = screenWidth / 2 - randomWidth / 2;
-              final top = screenHeight / 2.5 - randomHeight / 2;
-              return Positioned(
-                left: left,
-                top: top,
-                child: Transform.rotate(
-                  angle: randomAngle * pi / 180,
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedCard = kartenname;
-                      });
-                      _showCard(context);
-                    },
-                    child: Image.asset(
-                      'images/back.png',
-                      width: randomWidth.toDouble(),
-                      height: randomHeight.toDouble(),
-                    ),
-                  ),
-                ),
-              );
-            }).toList(),
-            // Anzeigen der ausgewählten Karte
-            if (selectedCard.isNotEmpty)
-              Positioned.fill(
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedCard = '';
-                    });
-                  },
-                  child: Container(
-                    color: Colors.black54,
-                    child: Center(
-                      child: Image.asset(
-                        'images/$selectedCard',
-                        height: 300.0,
-                      ),
-                    ),
-                  ),
+          IconButton(
+          icon: Icon(Icons.language, size: 40),
+            onPressed: () {
+              // Behandeln Sie den Klick auf das Menü-Symbol
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/background.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
-          ],
-        ),
+              child: Stack(
+                children: [
+                  // Anzeigen der Kartenrückseite
+                  ...karten.map((kartenname) {
+                    final randomAngle = Random().nextInt(90) - 45;
+                    return Positioned(
+                      left: left,
+                      top: top,
+                      child: Transform.rotate(
+                        angle: randomAngle * pi / 180,
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedCard = kartenname;
+                            });
+                            _showCard(context);
+                          },
+                          child: Image.asset(
+                            'images/back.png',
+                            width: cardWidth,
+                            height: cardHeight,
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  // Anzeigen der ausgewählten Karte
+                  if (selectedCard.isNotEmpty)
+                    Positioned.fill(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedCard = '';
+                          });
+                        },
+                        child: Container(
+                          color: Colors.black54,
+                          child: Center(
+                            child: Image.asset(
+                              'images/$selectedCard',
+                              height: 300.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  if (constraints.maxWidth > 600) {
+                    // code for wide screen
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // navigate to Datenschutzerklärung
+                          },
+                          child: const Text(
+                            'Datenschutzerklärung',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // navigate to Nutzungsbedingungen
+                          },
+                          child: const Text(
+                            'Nutzungsbedingungen',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // navigate to Kontakt
+                          },
+                          child: const Text(
+                            'Kontakt',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // navigate to Impressum
+                          },
+                          child: const Text(
+                            'Impressum',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    );
+                  } else {
+                    // code for small screen
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: PopupMenuButton<String>(
+                            itemBuilder: (BuildContext context) {
+                              return {
+                                'Datenschutzerklärung',
+                                'Nutzungsbedingungen',
+                                'Kontakt',
+                                'Impressum'
+                              }.map((String choice) {
+                                return PopupMenuItem<String>(
+                                  value: choice,
+                                  child: Text(
+                                    choice,
+                                    style: TextStyle(color: Colors.black),
+                                  ),
+                                );
+                              }).toList();
+                            },
+                            onSelected: (String choice) {
+                              // navigate to the selected page
+                            },
+                            icon: Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  // Anzeigen einer ausgewählten Karte in einer Dialogbox
+// Anzeigen einer ausgewählten Karte in einer Dialogbox
   void _showCard(BuildContext context) {
     String newCard;
     String newDesc;
@@ -198,7 +231,6 @@ class _CardPileState extends State<CardPile> {
     setState(() {
       selectedCard = newCard;
     });
-
     showDialog(
       context: context,
       builder: (BuildContext context) {
